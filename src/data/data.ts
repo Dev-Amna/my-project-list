d9c6e85e744719f920b91187183d37dbe820d73a
Dev-Amna <amnaharaf272@gmail.com>
2025-12-21 10:43:41 +0500

    Add data in Data.ts

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,59 +1,67 @@
-import pro1 from "../assets/rest-countries.jpg";
-import pro2 from "../assets/age-calculator-app.jpg";
-import pro3 from "../assets/contact-form.jpg";
-import pro4 from "../assets/Interactive-card-details-form.jpg";
-import pro5 from "../assets/todo-app.jpg";
-import pro6 from "../assets/Rocks-Paper-Scissors-.png";
-import pro7 from "../assets/Simon-Says-Game.png";
-import pro8 from "../assets/Calculator-app.jpg";
+import project1 from "../assets/project1.jpg";
+import project2 from "../assets/project2.jpg";
+import project3 from "../assets/project3.jpg";
+import project4 from "../assets/project4.jpg";
+import project5 from "../assets/project5.jpg";
+import project6 from "../assets/project6.png";
+import project7 from "../assets/project7.png";
+import project8 from "../assets/project8.jpg";
+import project9 from "../assets/project9.jpg";
+import project10 from "../assets/project10.jpg";
+import project11 from "../assets/project11.jpg";
+import project12 from "../assets/project12.jpg";
+// import project13 from "../assets/project13.jpg";
+// import project14 from "../assets/project14.jpg";
+// import project15 from "../assets/project15.jpg";
+// import project16 from "../assets/project16.jpg";
 
 const projects = [
   {
     id: 0,
-    status: "js,api",
+    status: "Js",
+    name: "Age-calculator",
+    img: project1,
+    liveDemo: "https://dev-amna.github.io/Age-calculator-app/",
+    code: "https://github.com/Dev-Amna/Age-calculator-app",
+  },
+  {
+    id: 1,
+    status: "js",
+    name: "Calculator",
+    img: project2,
+    liveDemo: "https://dev-amna.github.io/Calculator-app/",
+    code: "https://github.com/Dev-Amna/Calculator-app",
+  },
+  {
+    id: 2,
+    status: "Js",
+    name: "Interactive card details form",
+    img: project3,
+    liveDemo: "https://dev-amna.github.io/Interactive-card-details-form/",
+    code: "https://github.com/Dev-Amna/Interactive-card-details-form",
+  },
+  {
+    id: 3,
+    status: "react+vite",
+    name: "Agency landing page",
+    img: project4,
+    liveDemo: "https://agency-landing-page-ombk.vercel.app/",
+    code: "https://github.com/Dev-Amna/Agency-landing-page",
+  },
+  {
+    id: 4,
+    status: "Js,Api",
     name: " REST Countries API with color theme switcher",
-    img: pro1,
+    img: project5,
     liveDemo:
       "https://dev-amna.github.io/REST-Countries-API-with-color-theme-switcher/",
     code: "https://github.com/Dev-Amna/REST-Countries-API-with-color-theme-switcher",
   },
   {
-    id: 1,
-    status: "js,api",
-    name: " REST Countries API with color theme switcher",
-    img: pro2,
-    liveDemo: "https://dev-amna.github.io/Age-calculator-app/",
-    code: "https://github.com/Dev-Amna/Age-calculator-app",
-  },
-  {
-    id: 2,
-    status: "js,api",
-    name: " REST Countries API with color theme switcher",
-    img: pro3,
-    liveDemo: "https://dev-amna.github.io/Contact-form/",
-    code: "https://github.com/Dev-Amna/Contact-form",
-  },
-  {
-    id: 3,
-    status: "js,api",
-    name: " REST Countries API with color theme switcher",
-    img: pro4,
-    liveDemo: "https://dev-amna.github.io/Interactive-card-details-form/",
-    code: "https://github.com/Dev-Amna/Contact-form",
-  },
-  {
-    id: 4,
-    status: "js,api",
-    name: " REST Countries API with color theme switcher",
-    img: pro5,
-    liveDemo: "https://dev-amna.github.io/Todo--App/",
-    code: "https://github.com/Dev-Amna/Todo--App",
-  },
-  {
     id: 5,
     status: "Js",
     name: "Rocks-Paper-Scissors",
-    img: pro6,
+    img: project6,
     liveDemo: "https://dev-amna.github.io/Rocks-Paper-Scissors-/",
     code: "https://github.com/Dev-Amna/Rocks-Paper-Scissors-",
   },
@@ -61,54 +69,79 @@ const projects = [
     id: 6,
     status: "Js",
     name: "Simon-Says-Game",
-    img: pro7,
+    img: project7,
     liveDemo: "https://dev-amna.github.io/Simon-Says-Game/",
     code: "https://github.com/Dev-Amna/Simon-Says-Game",
   },
   {
     id: 7,
-    status: "Js",
-    name: "Calculator-app",
-    img: pro8,
-    liveDemo: "https://dev-amna.github.io/Calculator-app/",
-    code: "https://github.com/Dev-Amna/Calculator-app",
-  },{
+    status: "html,css,Js",
+    name: "Contact form",
+    img: project8,
+    liveDemo: "https://dev-amna.github.io/Contact-form/",
+    code: "https://github.com/Dev-Amna/Contact-form",
+  },
+  {
     id: 8,
-    status: "Js",
-    name: "Calculator-app",
-    img: pro8,
-    liveDemo: "https://dev-amna.github.io/Calculator-app/",
-    code: "https://github.com/Dev-Amna/Calculator-app",
-  },{
+    status: "Html,css,Js",
+    name: "Todo-app",
+    img: project9,
+    liveDemo: "https://dev-amna.github.io/Todo--App/",
+    code: "https://github.com/Dev-Amna/Todo--App",
+  },
+  {
     id: 9,
-    status: "Js",
-    name: "Calculator-app",
-    img: pro8,
-    liveDemo: "https://dev-amna.github.io/Calculator-app/",
-    code: "https://github.com/Dev-Amna/Calculator-app",
-  },{
+    status: "Js,Api,React+vite",
+    name: "Weather-app",
+    img: project10,
+    liveDemo: "https://weather-app-one-tau-93.vercel.app/",
+    code: "https://github.com/Dev-Amna/Weather-app",
+  },
+  {
     id: 10,
     status: "Js",
-    name: "Calculator-app",
-    img: pro8,
-    liveDemo: "https://dev-amna.github.io/Calculator-app/",
-    code: "https://github.com/Dev-Amna/Calculator-app",
-  },{
+    name: "Launch countdown timer",
+    img: project11,
+    liveDemo: "https://dev-amna.github.io/Launch-countdown-timer/",
+    code: "https://github.com/Dev-Amna/Launch-countdown-timer",
+  }
+  ,{
     id: 11,
-    status: "Js",
-    name: "Calculator-app",
-    img: pro8,
+    status: "Html,Css,Js",
+    name: "Pricing component with toggle",
+    img: project12,
     liveDemo: "https://dev-amna.github.io/Calculator-app/",
     code: "https://github.com/Dev-Amna/Calculator-app",
-  },{
-    id: 12,
-    status: "Js",
-    name: "Calculator-app",
-    img: pro8,
-    liveDemo: "https://dev-amna.github.io/Calculator-app/",
-    code: "https://github.com/Dev-Amna/Calculator-app",
-  },
+  }
+  // ,{
+  //   id: 12,
+  //   status: "Js",
+  //   name: "Calculator-app",
+  //   img: project13,
+  //   liveDemo: "https://dev-amna.github.io/Calculator-app/",
+  //   code: "https://github.com/Dev-Amna/Calculator-app",
+  // },{
+  //   id: 13,
+  //   status: "Js",
+  //   name: "Calculator-app",
+  //   img: project14,
+  //   liveDemo: "https://dev-amna.github.io/Calculator-app/",
+  //   code: "https://github.com/Dev-Amna/Calculator-app",
+  // },{
+  //   id: 14,
+  //   status: "Js",
+  //   name: "Calculator-app",
+  //   img: project15,
+  //   liveDemo: "https://dev-amna.github.io/Calculator-app/",
+  //   code: "https://github.com/Dev-Amna/Calculator-app",
+  // },{
+  //   id: 15,
+  //   status: "Js",
+  //   name: "Calculator-app",
+  //   img: project16,
+  //   liveDemo: "https://dev-amna.github.io/Calculator-app/",
+  //   code: "https://github.com/Dev-Amna/Calculator-app",
+  // },
 ];
-
 
 export default projects;
